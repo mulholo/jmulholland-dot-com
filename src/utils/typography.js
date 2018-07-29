df351f2a032827bcafe3027d088de05c3570f7f8
James Mulholland <jmulholland1618@gmail.com>
2018-07-29 18:04:48 +0100

    basic typography.js tweaks

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,12 @@
 import Typography from "typography";
 
 const typography = new Typography({
-  baseFontSize: "16px"
+  baseFontSize: "16px",
+  baseLineHeight: 1.5,
+  headerFontFamily: [ "SF Pro Text","SF Pro Icons", "Helvetica Neue","Helvetica","Arial", 'sans-serif' ],
+  bodyFontFamily: [ "SF Pro Text","SF Pro Icons", "Helvetica Neue","Helvetica","Arial", 'sans-serif' ]
 });
+
+
 
 export default typography;
