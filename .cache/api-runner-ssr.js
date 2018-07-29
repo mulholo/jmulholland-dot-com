21fcdc0cdc562ff456bba48bee745e2c9ee05b28
James Mulholland <jmulholland1618@gmail.com>
2018-07-29 17:22:23 +0100

    add typography.js

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -1,4 +1,7 @@
-var plugins = []
+var plugins = [{
+      plugin: require('/Users/jamesmulholland/proj/jmulholland-dot-com/node_modules/gatsby-plugin-typography/gatsby-ssr'),
+      options: {"plugins":[],"pathToConfigModule":"src/utils/typography.js"},
+    }]
 // During bootstrap, we write requires at top of this file which looks like:
 // var plugins = [
 //   {
