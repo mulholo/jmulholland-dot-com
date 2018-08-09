2357596f16d5efe293cb95277f453db7ad4d1f94
James Mulholland <james@jmulholland.com>
2018-08-09 14:39:29 +0100

    fix .gitignore

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/jamesmulholland/proj/jmulholland-dot-com/src/layouts/index.jsx"
-  import data from "/Users/jamesmulholland/proj/jmulholland-dot-com/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
