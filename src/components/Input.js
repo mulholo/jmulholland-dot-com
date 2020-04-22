f82309f870065b6f91b58d7889a3382dca71763f
mulholio <james@jmulholland.com>
2020-04-22 09:09:56 +0100

    update input focus styles

--- src/components/Input.js
+++ src/components/Input.js
@@ -4,11 +4,21 @@ const Input = styled.input(
   ({ theme }) => css`
     background: ${theme.colors.n900};
     border: 1px solid ${theme.colors.n500};
-    color: ${theme.colors.n100};
+    color: ${theme.colors.n200};
     flex: 1 1 0;
     font-size: ${theme.fontSizes[2]};
     margin: 0;
     padding: ${theme.space[4]} ${theme.space[4]};
+
+    transition: 0.2s border, 0.2s box-shadow;
+
+    &:active,
+    &:focus {
+      outline: none;
+      border: 1px solid ${theme.colors.b500};
+      box-shadow: inset 0px 0px 0px 3px
+        ${({ theme }) => theme.colors.b500};
+    }
   `
 )
 
