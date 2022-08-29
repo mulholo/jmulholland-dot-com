e78169edfc05500b0d763d5daf7c16658ff9a43a
James Mulholland <james@attio.com>
2022-08-29 09:22:51 +0100

    close menu on click outside

---
+++ src/components/icons.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+export const BurgerIcon = () => (
+  <svg width='24' height='24' fill='none' viewBox='0 0 24 24'>
+    <path
+      stroke='currentColor'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      strokeWidth='1.5'
+      d='M4.75 5.75H19.25'
+    ></path>
+    <path
+      stroke='currentColor'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      strokeWidth='1.5'
+      d='M4.75 18.25H19.25'
+    ></path>
+    <path
+      stroke='currentColor'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      strokeWidth='1.5'
+      d='M4.75 12H19.25'
+    ></path>
+  </svg>
+)
+
+export const CloseIcon = () => (
+  <svg width='24' height='24' fill='none' viewBox='0 0 24 24'>
+    <path
+      stroke='currentColor'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      strokeWidth='1.5'
+      d='M17.25 6.75L6.75 17.25'
+    ></path>
+    <path
+      stroke='currentColor'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      strokeWidth='1.5'
+      d='M6.75 6.75L17.25 17.25'
+    ></path>
+  </svg>
+)
