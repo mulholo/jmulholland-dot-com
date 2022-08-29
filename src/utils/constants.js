19e8e850ce39aa1cf995f58b2182434f6e2145f6
James Mulholland <james@attio.com>
2022-08-29 10:28:33 +0100

    tweak layout

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,13 +2,7 @@
  * The point above which the header is presented on the side of the
  * screen
  */
-export const HEADER_SWITCH_SIZE = `${760 / 16}em`
-
-/**
- * The point above which main text content gets footnotes, asides, etc
- * presented in a side column
- */
-export const CONTENT_SIDEBAR_SWITCH_SIZE = `${920 / 16}em`
+export const HEADER_SWITCH_SIZE = `${1200 / 16}rem`
 
 export const RATIO = 1.618
 
