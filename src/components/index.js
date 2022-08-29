33738d9aece5a49ada89af18339c55e6369312cd
James Mulholland <james@attio.com>
2022-08-29 10:06:52 +0100

    clean ups

--- src/components/index.js
+++ src/components/index.js
@@ -3,9 +3,14 @@ export { default as Button } from './Button'
 export { default as Concept } from './Concept'
 export { default as Detail } from './Detail'
 export { Cluster } from './Cluster'
-export { Footnote, FootnoteStack, FootnoteParagraphWrapper } from "./Footnote"
+export {
+  Footnote,
+  FootnoteStack,
+  FootnoteParagraphWrapper,
+} from './Footnote'
 export { default as Grid } from './Grid'
 export { default as Input } from './Input'
+export * as Icon from './Icon'
 export { Layout } from './Layout'
 export { Separator } from './Separator'
 export { default as Sidebar } from './Sidebar'
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,16 @@
+/**
+ * The point above which the header is presented on the side of the
+ * screen
+ */
 export const HEADER_SWITCH_SIZE = `${760 / 16}em`
-export const CONTENT_SIDEBAR_SWITCH_SIZE = `${760 / 16}em`
+
+/**
+ * The point above which main text content gets footnotes, asides, etc
+ * presented in a side column
+ */
+export const CONTENT_SIDEBAR_SWITCH_SIZE = `${920 / 16}em`
 
 export const RATIO = 1.618
 
-export const YOUTUBE_LINK = "https://www.youtube.com/channel/UCoM-EVJlUBxoGdSkyQaEH4w"
+export const YOUTUBE_LINK =
+  'https://www.youtube.com/channel/UCoM-EVJlUBxoGdSkyQaEH4w'
