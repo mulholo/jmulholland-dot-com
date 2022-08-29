cbcde023e1cdba2b5ea49835a9c61b085914a68e
James Mulholland <james@attio.com>
2022-08-29 10:08:16 +0100

    extract useScroll

--- src/templates/BlogPost.jsx
+++ src/templates/BlogPost.jsx
@@ -1,38 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
 import { graphql } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
-import debounce from 'lodash.debounce'
-import { RATIO, track } from '../utils'
+import { useScroll, RATIO, track } from '../utils'
 import { Detail, Layout, TextContainer } from '../components'
-
-/**
- * Returns amount of page scrolled as a percentage
- */
-function useScroll() {
-  const calcPctComplete = () => {
-    if (typeof window === 'undefined') return 0
-    const winScroll =
-      document.body.scrollTop || document.documentElement.scrollTop
-    const height =
-      document.documentElement.scrollHeight -
-      document.documentElement.clientHeight
-    return (winScroll / height) * 100
-  }
-
-  const [pctComplete, setPctComplete] = useState(calcPctComplete())
-
-  useEffect(() => {
-    const listener = debounce(
-      () => setPctComplete(calcPctComplete()),
-      100
-    )
-    window.addEventListener('scroll', listener)
-    return () => window.removeEventListener('scroll', listener)
-  }, [setPctComplete, calcPctComplete])
-
-  return pctComplete
-}
 
 const Header = styled.header`
   margin-bottom: ${({ theme }) => theme.sizes.s2};
--- src/utils/index.js
+++ src/utils/index.js
@@ -2,4 +2,5 @@ export { theme } from './theme'
 export { fadeIn } from "./fadeIn"
 export { track } from './track'
 export { useClickOutside } from './useClickOutside'
+export { useScroll } from './useScroll'
 export * from './constants'
