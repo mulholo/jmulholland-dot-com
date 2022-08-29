206b80e8b80e5b931ed5ca79043017f38c63d8e3
James Mulholland <james@attio.com>
2022-08-29 09:49:11 +0100

    clean ups

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -21,64 +21,55 @@ const FooterLink = ({ href, title }) => (
   </li>
 )
 
-// Footer is wrapped in a div to avoid collapse issue on Safari
-
 const Footer = () => (
-  <div>
-    <footer
-      css={`
-        background: ${(props) => props.theme.colors.n900};
-        border-top: 1px solid ${(props) => props.theme.colors.n300};
-        font-family: ${(props) => props.theme.fonts.sans};
-        display: flex;
-        flex-wrap: wrap;
-        align-items: baseline;
-        padding: ${({ theme }) => theme.sizes.s1}
-          ${({ theme }) => theme.sizes.s2}
-          ${({ theme }) => theme.sizes.s2}
-          ${({ theme }) => theme.sizes.s2};
+  <footer
+    css={`
+      grid-area: footer;
+      background: ${(props) => props.theme.colors.n900};
+      border-top: 1px solid ${(props) => props.theme.colors.n300};
+      font-family: ${(props) => props.theme.fonts.sans};
+      display: flex;
+      flex-wrap: wrap;
+      align-items: baseline;
+      padding: ${({ theme }) => theme.sizes.s1}
+        ${({ theme }) => theme.sizes.s2}
+        ${({ theme }) => theme.sizes.s2}
+        ${({ theme }) => theme.sizes.s2};
 
+      @media (min-width: ${450 / 16}em) {
+        padding: ${({ theme }) => theme.sizes.s1}
+          ${({ theme }) => theme.sizes.s2};
+      }
+    `}
+  >
+    <ul
+      css={`
+        margin: 0;
+        padding: 0;
+        list-style: none;
+        display: flex;
+        flex: 1;
+        & * > * {
+          margin-right: ${({ theme }) => theme.sizes['s-1']};
+        }
+        flex-direction: column;
         @media (min-width: ${450 / 16}em) {
-          padding: ${({ theme }) => theme.sizes.s1}
-            ${({ theme }) => theme.sizes.s2};
+          flex-direction: row;
         }
       `}
     >
-      <ul
-        css={`
-          margin: 0;
-          padding: 0;
-          list-style: none;
-          display: flex;
-          flex: 1;
-          & * > * {
-            margin-right: ${({ theme }) => theme.sizes['s-1']};
-          }
-          flex-direction: column;
-          @media (min-width: ${450 / 16}em) {
-            flex-direction: row;
-          }
-        `}
-      >
-        <FooterLink
-          title='GitHub'
-          href='https://github.com/mulholo'
-        />
-        <FooterLink
-          title='Twitter'
-          href='https://twitter.com/mulholo'
-        />
-        <FooterLink
-          title='LinkedIn'
-          href='https://www.linkedin.com/in/mulholo'
-        />
-        <FooterLink
-          title='Email'
-          href='mailto:james@jmulholland.com'
-        />
-      </ul>
-    </footer>
-  </div>
+      <FooterLink title='GitHub' href='https://github.com/mulholo' />
+      <FooterLink
+        title='Twitter'
+        href='https://twitter.com/mulholo'
+      />
+      <FooterLink
+        title='LinkedIn'
+        href='https://www.linkedin.com/in/mulholo'
+      />
+      <FooterLink title='Email' href='mailto:james@jmulholland.com' />
+    </ul>
+  </footer>
 )
 
 export default Footer
