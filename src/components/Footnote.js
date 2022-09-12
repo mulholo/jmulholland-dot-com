b15600402b91ae11943348ca7ae8a3c66d1fb1af
James Mulholland <james@attio.com>
2022-09-12 21:40:09 +0100

    slide-in footnotes!

--- src/components/Footnote.js
+++ src/components/Footnote.js
@@ -2,6 +2,8 @@ import React from 'react'
 import styled from 'styled-components'
 import { RATIO, HEADER_SWITCH_SIZE } from '../utils'
 
+const FootnoteContext = React.createContext()
+
 const FootnoteContainer = styled.aside`
   position: relative;
   &:not(:last-child) {
@@ -17,22 +19,55 @@ const N = styled.span`
   font-weight: ${({ theme }) => theme.fontWeights.semiBold};
 `
 
-export const FootnoteN = styled.sup`
+const FootnoteNContainer = styled.sup`
   color: ${({ theme }) => theme.colors.b400};
   font-weight: ${({ theme }) => theme.fontWeights.semiBold};
   font-family: ${({ theme }) => theme.fonts.sans};
   font-size: ${({ theme }) => theme.fontSizes['s-1']};
 `
 
-export const Footnote = ({ children, n }) => (
-  <FootnoteContainer>
-    {n && <N>{n}</N>}
-    {children}
-  </FootnoteContainer>
-)
+const FootnoteNButton = styled.button`
+  // reset
+  background: none;
+  border: none;
+  text-align: left;
+  margin: 0;
 
-export const FootnoteStack = styled.div`
+  &:active,
+  &:focus {
+    outline: none;
+  }
+`
+
+export function FootnoteN({ children, className }) {
+  const { toggleIsOpen } = React.useContext(FootnoteContext)
+
+  return (
+    <FootnoteNButton onClick={toggleIsOpen} className={className}>
+      <FootnoteNContainer>{children}</FootnoteNContainer>
+    </FootnoteNButton>
+  )
+}
+
+export const Footnote = ({ children, n, className }) => {
+  return (
+    <FootnoteContainer className={className}>
+      {n && <N>{n}</N>}
+      {children}
+    </FootnoteContainer>
+  )
+}
+
+const FootnoteStackContainer = styled.button`
   &&& {
+    // reset
+    background: none;
+    border: none;
+    outline: none;
+    text-align: left;
+
+    cursor: pointer;
+
     margin: 0 0 0 ${({ theme }) => theme.sizes.s1};
     position: absolute;
     top: 5px;
@@ -48,22 +83,64 @@ export const FootnoteStack = styled.div`
     @media (min-width: ${HEADER_SWITCH_SIZE}) {
       left: ${(1 / RATIO) * 100}%;
       right: 0;
+      cursor: inherit;
     }
   }
 `
 
-export const FootnoteParagraphWrapper = styled.div`
+export function FootnoteStack({ children, className }) {
+  const { toggleIsOpen } = React.useContext(FootnoteContext)
+
+  return (
+    <FootnoteStackContainer
+      className={className}
+      onClick={toggleIsOpen}
+    >
+      {children}
+    </FootnoteStackContainer>
+  )
+}
+
+const FootnoteParagraphWrapperContainer = styled.div`
   && {
     position: relative;
     overflow-y: initial;
     overflow-x: initial;
 
+    transform: translateX(
+      ${({ $isOpen, theme }) => ($isOpen ? `calc(-100% - ${theme.sizes.s1})` : `0`)}
+    );
+    transition: 0.25s transform ease-in-out;
+
+    & > *:not(${FootnoteStackContainer}) { 
+      margin-right: 0;
+    }
+
     @media (min-width: ${HEADER_SWITCH_SIZE}) {
       width: 100%;
+      transform: none;
 
-      & > *:not(${FootnoteStack}) {
+      & > *:not(${FootnoteStackContainer}) {
         margin-right: ${(1 - 1 / RATIO) * 100}%;
+        max-width: 100%;
       }
     }
   }
 `
+
+export function FootnoteParagraphWrapper({ children, className }) {
+  const [isOpen, setIsOpen] = React.useState(false)
+
+  const toggleIsOpen = () => setIsOpen((b) => !b)
+
+  return (
+    <FootnoteContext.Provider value={{ isOpen, toggleIsOpen }}>
+      <FootnoteParagraphWrapperContainer
+        className={className}
+        $isOpen={isOpen}
+      >
+        {children}
+      </FootnoteParagraphWrapperContainer>
+    </FootnoteContext.Provider>
+  )
+}
