b79797f5d6d47251651075011d1ffc3e2733f215
James Mulholland <james@attio.com>
2023-02-11 14:42:39 +0000

    update cluster

--- src/components/Cluster.js
+++ src/components/Cluster.js
@@ -1,18 +1,17 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 export const Cluster = styled.div(
-  ({ theme, space = 's0' }) => `
+  ({ theme, space = 's0' }) => css`
+    & > * {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: flex-start;
+      align-items: center;
+      margin: calc(${theme.sizes[space]} / 2 * -1);
+    }
 
-  & > * {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: flex-start;
-    align-items: center;
-    margin: calc(${theme.sizes[space]} / 2 * -1);
-  }
-
-  & > * > * {
-    margin: calc(${theme.sizes[space]} / 2);
-  }
-`
+    & > * > * {
+      margin: calc(${theme.sizes[space]} / 2);
+    }
+  `
 )
